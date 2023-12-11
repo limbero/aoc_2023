b720f264318acfb2d7b3877c91f7444cc1e7ebe5
Axel Samuelsson <samuelsson.axel@gmail.com>
2023-12-11 18:09:16 +0100

    dag 11 ez

---
+++ 11/solver_second.py
@@ -0,0 +1,83 @@
+def main():
+  data_file = open('data.txt', 'r')
+  lines = data_file.read().splitlines()
+
+  galaxies = []
+  xdx = 0
+  while xdx < len(lines[0]):
+    ydx = 0
+    while ydx < len(lines):
+      if lines[ydx][xdx] != ".":
+        galaxies.append({"x": xdx, "y": ydx})
+      ydx += 1
+    xdx += 1
+  
+  distances_normal = 0
+  while len(galaxies) > 0:
+    this_galaxy = galaxies[0]
+    galaxies.remove(this_galaxy)
+    for other_galaxy in galaxies:
+      distance = abs(this_galaxy["x"] - other_galaxy["x"])
+      distance += abs(this_galaxy["y"] - other_galaxy["y"])
+      distances_normal += distance
+
+  ydx = 0
+  empty_rows = []
+  while ydx < len(lines):
+    alldots = True
+    xdx = 0
+    while xdx < len(lines[0]):
+      if lines[ydx][xdx] != ".":
+        alldots = False
+        break
+      xdx += 1
+    if alldots:
+      empty_rows.insert(0, ydx)
+    ydx += 1
+
+  xdx = 0
+  empty_cols = []
+  while xdx < len(lines[0]):
+    alldots = True
+    ydx = 0
+    while ydx < len(lines):
+      if lines[ydx][xdx] != ".":
+        alldots = False
+        break
+      ydx += 1
+    if alldots:
+      empty_cols.insert(0, xdx)
+    xdx += 1
+  
+  for col in empty_cols:
+    for idx, line in enumerate(lines):
+      lines[idx] = line[:col] + "." + line[col:]
+  
+  for row in empty_rows:
+    lines.insert(row, lines[row])
+
+  galaxies = []
+  xdx = 0
+  while xdx < len(lines[0]):
+    ydx = 0
+    while ydx < len(lines):
+      if lines[ydx][xdx] != ".":
+        galaxies.append({"x": xdx, "y": ydx})
+      ydx += 1
+    xdx += 1
+  
+  distances_expanded_one = 0
+  while len(galaxies) > 0:
+    this_galaxy = galaxies[0]
+    galaxies.remove(this_galaxy)
+    for other_galaxy in galaxies:
+      distance = abs(this_galaxy["x"] - other_galaxy["x"])
+      distance += abs(this_galaxy["y"] - other_galaxy["y"])
+      distances_expanded_one += distance
+
+  expansion_diff = distances_expanded_one - distances_normal
+  distances_expanded_million = distances_normal + expansion_diff*999999
+  print(distances_expanded_million)
+
+if __name__ == "__main__":
+    main()
